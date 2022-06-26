cac95ca01b8df81aa25cf5396189c4a69a3cacc9
DanVi <danicrisa@hotmail.com>
2022-06-25 21:22:52 -0300

    adicionada feature aleatória e remoção de imagem prévia

--- dogAPI/script.js
+++ dogAPI/script.js
@@ -1,6 +1,14 @@
 const container = document.getElementById('demo')
+const botaoDogEspecifico = document.getElementById('especifico')
+const botaoDogAleatorio = document.getElementById('aleatorio')
 
-async function getDoguinho(breed) {
+const limparConteudo = () => {container.removeChild(container.firstChild)}
+
+botaoDogAleatorio.addEventListener("click", () => {container.removeChild(container.firstChild)}, false);
+botaoDogEspecifico.addEventListener("click", () => {container.removeChild(container.firstChild)}, false);
+
+async function getDoguinhoEspecifico(){
+  var breed = prompt('Escolha uma raça de cachorro: ');
   try {
     const resposta = await fetch(`https://dog.ceo/api/breed/${breed}/images/random`)
     // console.log(resposta)
@@ -11,8 +19,22 @@ async function getDoguinho(breed) {
     container.appendChild(doguinhoImagem)
   }
   catch(err) {
-    console.error("HTTP Error: ", err)
+    console.error("HTTP Error: ", err);
+    alert('Essa raça não existe na nossa base de raças');
   }
 }
 
-getDoguinho('basenji')
+async function getDoguinhoAleatorio(){
+  try{
+    const resposta = await fetch('https://dog.ceo/api/breeds/image/random')
+    const dados = await resposta.json()
+    console.log(dados)
+    const doguinhoImagem = document.createElement('img')
+    doguinhoImagem.setAttribute('src', dados.message)
+    container.appendChild(doguinhoImagem)
+  }
+  catch(err){
+    console.error("HTTP Error: ", err);
+    alert('Essa raça não existe na nossa base de raças');
+  }
+}
